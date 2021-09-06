31caed54eaa28dbb73619b63ff10ada7e6178bfa
LuizGGoncalves <l.cgoncalvea@gmail.com>
2021-09-06 14:12:52 -0300

    commit inicial

---
+++ src/main/java/com/Goncadex/Goncadex/GoncadexApplication.java
@@ -0,0 +1,13 @@
+package com.Goncadex.Goncadex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoncadexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoncadexApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Goncadex/Goncadex/GoncadexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Goncadex.Goncadex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoncadexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
